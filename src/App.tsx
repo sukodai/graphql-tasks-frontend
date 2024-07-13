06a5c5052a452ae6e85b576f514369649dcf9494
sukodai <sukodai@gmail.com>
2024-07-14 07:43:29 +0900

    added components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import "./App.css";
+import NotFound from "./components/NotFound";
+import Main from "./components/Main";
+import SignIn from "./components/SignIn";
+import SignUp from "./components/SignUp";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h1>HelloWorld.</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/" element={<Main />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
